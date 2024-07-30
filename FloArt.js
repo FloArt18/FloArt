6a5d84ded611a9489045dfda14a2d2858a10f25f
FloArt18 <floagent@yahoo.com>
2024-07-30 22:58:25 +0300

    Update FloArt.js

--- FloArt.js
+++ FloArt.js
@@ -25,7 +25,7 @@
             }
         });
     });
-});
+}); */
 /* scroll top page.js */
 
 // Get the button element
@@ -48,7 +48,7 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-} */
+} 
 
 
 
